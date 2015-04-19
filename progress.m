368ebe53be28d28f00f685090081921bcc91d531
Asif <asif.ahmad@gmx.com>
2015-04-19 01:54:21 +0100

    for florence: change scratch disk

--- progress.m
+++ progress.m
@@ -26,28 +26,13 @@ else
     fprintf(formatSpec2,currentIndex,finalIndex,completedPercentage,completionTime(4),completionTime(5),datestr(completionTime,'mmm'),completionTime(3),averageTime,currentItem);
 end
 
-    delete(fullfile('/run/shm','ETC*'))
-    logFileName = fullfile('/run/shm',sprintf(formatSpec3,completionTime(4),completionTime(5),...
-            datestr(completionTime,'mmm'),completionTime(3),currentIndex,finalIndex,completedPercentage,floor(averageTime+0.5)));
+    delete(fullfile('~','ETC*'))
+    logFileName = fullfile('~',sprintf(formatSpec3,completionTime(4),completionTime(5),...
+            datestr(completionTime,'mmm'),completionTime(3),currentIndex,finalIndex,floor(completedPercentage+0.5),floor(averageTime+0.5)));
     logFileID = fopen(logFileName,'w');
     fclose(logFileID);   
 
 
-
-% logFileName = fullfile('/run/shm',sprintf(formatSpec4,currentIndex,finalIndex,completionTime(4),completionTime(5),...
-%         datestr(completionTime,'mmm'),completionTime(3),elapsedTimeVector(4),elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime))
-% 
-% logFileID = fopen(fullfile('/run/shm',logFileName,'.q'),'w');
-% fclose(logFileID);   
-% elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime)
-
-
-
-% logFileName = fullfile('/run/shm',sprintf(formatSpec3,finalIndex,completionTime(4),completionTime(5),...
-%         datestr(completionTime,'mmm'),completionTime(3),elapsedTimeVector(4),elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime));
-% delete(fullfile('/run/shm','*.q'))
-% logFileID = fopen(fullfile('/run/shm',logfileName,'.q'),'w');
-% fclose(logFileID);   
 
 if currentIndex == finalIndex
     fprintf('Elapsed time = %02u:%02u:%02u \n',elapsedTimeVector(4),elapsedTimeVector(5),floor(elapsedTimeVector(6)))

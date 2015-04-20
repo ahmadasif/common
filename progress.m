16eb97c74d4c957016a62f3c03a2b019fd3ae0f0
Asif <asif.ahmad@gmx.com>
2015-04-20 20:47:25 +0100

    generalize log dir + update on pct change

--- progress.m
+++ progress.m
@@ -1,7 +1,10 @@
 function startTime = progress(currentItem,currentIndex,finalIndex,beginTime);
 
-if currentIndex == 0   
-    fprintf('Beginning Loop...\n')
+logFileDir = '/run/shm';
+
+if currentIndex == 0
+    delete(fullfile(logFileDir,'ETC*'))
+    fprintf('Beginning Loop...\n\n')
     startTime = now;
     return;
 end
@@ -19,29 +22,31 @@ currentTime = datevec(now);
 formatSpec1 = '%u of %u (%3.1f%%) - ETC %02u:%02u (Avg=%3.2fs) - %s\n';
 formatSpec2 = '%u of %u (%3.1f%%) - ETC %02u:%02u (%s %u) (Avg=%3.2fs) - %s\n';
 formatSpec3 = 'ETC%02u%02u%s%u~%u-%u-%upct~avg=%ds';
-formatSpec4 = 'Completed %u at %02u%02u on %s %u ~ elapsed time - %02u:%02u:%02u ~ avg=%ds';
+formatSpec4 = '\nCompleted %u at %02u:%02u on %s %u ~ elapsed time - %02u:%02u:%02u ~ avg=%3.1fs';
 if currentTime(3) == completionTime(3)
     fprintf(formatSpec1,currentIndex,finalIndex,completedPercentage,completionTime(4),completionTime(5),averageTime,currentItem);
 else
     fprintf(formatSpec2,currentIndex,finalIndex,completedPercentage,completionTime(4),completionTime(5),datestr(completionTime,'mmm'),completionTime(3),averageTime,currentItem);
 end
 
-    delete(fullfile('~','ETC*'))
-    logFileName = fullfile('~',sprintf(formatSpec3,completionTime(4),completionTime(5),...
+    
+
+previousCompletedPercentage = (currentIndex - 1) / finalIndex * 100;
+if floor(completedPercentage) > floor(previousCompletedPercentage)
+    delete(fullfile(logFileDir,'ETC*'))
+    logFileName = fullfile(logFileDir,sprintf(formatSpec3,completionTime(4),completionTime(5),...
             datestr(completionTime,'mmm'),completionTime(3),currentIndex,finalIndex,floor(completedPercentage+0.5),floor(averageTime+0.5)));
     logFileID = fopen(logFileName,'w');
     fclose(logFileID);   
-
-
+end
+    
+    
 
 if currentIndex == finalIndex
-    fprintf('Elapsed time = %02u:%02u:%02u \n',elapsedTimeVector(4),elapsedTimeVector(5),floor(elapsedTimeVector(6)))
-    
-%     logFileName = fullfile('/run/shm',sprintf(formatSpec3,finalIndex,completionTime(4),completionTime(5),...
-%         datestr(completionTime,'mmm'),completionTime(3),elapsedTimeVector(4),elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime));
-% 
-%     logFileID = fopen(fullfile('/run/shm',logFileName,'.q'),'w');
-%     fclose(logFileID);   
+    fprintf(formatSpec4,finalIndex,completionTime(4),completionTime(5),...
+         datestr(completionTime,'mmm'),completionTime(3),elapsedTimeVector(4),...
+         elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime);
+
 end
     
 

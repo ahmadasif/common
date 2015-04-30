e49d8d00909513525adfa021308c9c2f8c6e710f
Asif <asif.ahmad@gmx.com>
2015-04-30 15:46:56 +0100

    added second log - logfile.txt

--- progress.m
+++ progress.m
@@ -1,6 +1,7 @@
 function startTime = progress(currentItem,currentIndex,finalIndex,beginTime);
 
 logFileDir = '~';
+logFile2Name = fullfile(logFileDir,'logfile.txt');
 
 if currentIndex == 0
     delete(fullfile(logFileDir,'ETC*'))
@@ -22,31 +23,54 @@ currentTime = datevec(now);
 formatSpec1 = '%u of %u (%3.1f%%) - ETC %02u:%02u (Avg=%3.2fs) - %s\n';
 formatSpec2 = '%u of %u (%3.1f%%) - ETC %02u:%02u (%s %u) (Avg=%3.2fs) - %s\n';
 formatSpec3 = 'ETC%02u%02u%s%u~%u-%u-%upct~avg=%3.2fs';
-formatSpec4 = '\nCompleted %u at %02u:%02u on %s %u ~ elapsed time - %02u:%02u:%02u ~ avg=%3.1fs';
+formatSpec4 = '%u/%u\n%3.1f%%\nAvg %3.2fs\nETC %02u:%02u\n%s %u\n';
+formatSpec5 = ...
+    '\nCompleted %u at %02u:%02u on %s %u ~ elapsed time - %02u:%02u:%02u ~ avg=%3.1fs';
+formatSpec6 = 'Completed\n%u\nAvg %3.2fs\nat %02u:%02u\non %s %u\n';
+formatSpec7 = 'ETCompleted%u~Avg=%3.2fs~%02u%02u(%s%u)';
 if currentTime(3) == completionTime(3)
-    fprintf(formatSpec1,currentIndex,finalIndex,completedPercentage,completionTime(4),completionTime(5),averageTime,currentItem);
+    fprintf(formatSpec1,currentIndex,finalIndex,completedPercentage...
+        ,completionTime(4),completionTime(5),averageTime,currentItem);
 else
-    fprintf(formatSpec2,currentIndex,finalIndex,completedPercentage,completionTime(4),completionTime(5),datestr(completionTime,'mmm'),completionTime(3),averageTime,currentItem);
+    fprintf(formatSpec2,currentIndex,finalIndex,completedPercentage...
+        ,completionTime(4),completionTime(5),datestr(completionTime,'mmm')...
+        ,completionTime(3),averageTime,currentItem);
 end
 
     
 
 previousCompletedPercentage = (currentIndex - 1) / finalIndex * 100;
 if floor(completedPercentage) > floor(previousCompletedPercentage)
-    delete(fullfile(logFileDir,'ETC*'))
-    logFileName = fullfile(logFileDir,sprintf(formatSpec3,completionTime(4),completionTime(5),...
-            datestr(completionTime,'mmm'),completionTime(3),currentIndex,finalIndex,floor(completedPercentage+0.5),averageTime));
-    logFileID = fopen(logFileName,'w');
-    fclose(logFileID);   
+    delete(fullfile(logFileDir,'ETC*'));
+    logFile1Name = fullfile(logFileDir,sprintf(formatSpec3,completionTime(4)...
+        ,completionTime(5),datestr(completionTime,'mmm'),completionTime(3)...
+        ,currentIndex,finalIndex,floor(completedPercentage+0.5),averageTime));
+    logFile1ID = fopen(logFile1Name,'w');
+    fclose(logFile1ID);
+    logFile2ID = fopen(logFile2Name,'w');
+    fprintf(logFile2ID,formatSpec4,currentIndex,finalIndex,completedPercentage...
+        ,averageTime,completionTime(4),completionTime(5)...
+        ,datestr(completionTime,'mmm'),completionTime(3));
+    fclose(logFile2ID);
 end
     
     
 
 if currentIndex == finalIndex
-    fprintf(formatSpec4,finalIndex,completionTime(4),completionTime(5),...
-         datestr(completionTime,'mmm'),completionTime(3),elapsedTimeVector(4),...
-         elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime);
-
+    fprintf(formatSpec5,finalIndex,completionTime(4),completionTime(5)...
+        ,datestr(completionTime,'mmm'),completionTime(3),elapsedTimeVector(4)...
+        ,elapsedTimeVector(5),floor(elapsedTimeVector(6)),averageTime);
+    logFile2ID = fopen(logFile2Name,'w');
+    fprintf(logFile2ID,formatSpec6,finalIndex...
+        ,averageTime,completionTime(4),completionTime(5)...
+        ,datestr(completionTime,'mmm'),completionTime(3));
+    fclose(logFile2ID);
+    delete(fullfile(logFileDir,'ETC*'));
+    logFile1Name = fullfile(logFileDir,sprintf(formatSpec7,finalIndex...
+        ,averageTime,completionTime(4),completionTime(5)...
+        ,datestr(completionTime,'mmm'),completionTime(3)));
+    logFile1ID = fopen(logFile1Name,'w');
+    fclose(logFile1ID);
 end
     
 
